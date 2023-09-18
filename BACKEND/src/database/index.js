8f07f4130f1352aad5de736a6172aa4540d6eda2
DeoclecioR <deocleciorodrigues15@gmail.com>
2023-09-18 16:31:48 -0300

    Atualizando  link de acesso

--- BACKEND/src/database/index.js
+++ BACKEND/src/database/index.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 //Para mudar o servidor, basta alterar a connectionString para a sua
 //Caso haja erro de conexão certifique que seu IP está registrado na lista de IPs válidos do banco de dados
 const connectionString = 
-'mongodb+srv://dedev:IMsnxD0th18YxNBa@cluster0.mi1p9ay.mongodb.net/?retryWrites=true&w=majority';
+'mongodb+srv://deocleciorodrigues15:7THaxVCvaQzgtHVr@test.leeep01.mongodb.net/?retryWrites=true&w=majorityongodb+srv://dedev:IMsnxD0th18YxNBa@cluster0.mi1p9ay.mongodb.net/?retryWrites=true&w=majority';
 
 //Conexão
 mongoose
